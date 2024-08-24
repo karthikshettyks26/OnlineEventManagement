83be44a71adbc518a810cefa024a4140b68a4d76
karthikshettyks26 <karthikshettyks26@gmail.com>
2024-08-24 21:08:21 +0530

    Added post api fror event register

---
+++ OnlineEventManagement/Controllers/EventRegisterController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEventManagement.Repositories;
+
+namespace OnlineEventManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventRegisterController : ControllerBase
+    {
+        private readonly IEventRegisterRepository eventRegisterRepository;
+        private readonly IMapper mapper;
+
+        public EventRegisterController(IEventRegisterRepository eventRegisterRepository, IMapper mapper)
+        {
+            this.eventRegisterRepository = eventRegisterRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        //CREATE EVENT REGISTER
+        public async Task<IActionResult> CreateEventRegister([FromRoute] Guid eventId)
+        {
+            //ToDo - current user
+            Guid createdBy = Guid.NewGuid();
+            Guid? eventRegisterId = await eventRegisterRepository.AddEventRegisterAsync(eventId, createdBy);
+
+            if(eventRegisterId == null)
+                return Ok("Event has no more slots!!");
+
+            return Ok("You are registered to event successfully!");
+        }
+    }
+}
--- OnlineEventManagement/Repositories/EventRegisterRepository.cs
+++ OnlineEventManagement/Repositories/EventRegisterRepository.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Data.SqlClient;
+using System.Data;
+
 namespace OnlineEventManagement.Repositories
 {
     public class EventRegisterRepository : IEventRegisterRepository
@@ -9,9 +12,55 @@ namespace OnlineEventManagement.Repositories
         {
             this.configuration = configuration;
         }
-        public Task<Guid?> AddEventRegisterAsync(Guid eventId, Guid createdBy)
+
+        /// <summary>
+        /// Add Event register
+        /// </summary>
+        /// <param name="eventId"></param>
+        /// <param name="createdBy"></param>
+        /// <returns></returns>
+        public async Task<Guid?> AddEventRegisterAsync(Guid eventId, Guid createdBy)
         {
-            throw new NotImplementedException();
+            Guid? newRegisterId = null;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+                {
+                    await connection.OpenAsync();
+
+                    using (SqlCommand command = new SqlCommand("sp_AddEventRegister", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // Add parameters
+                        command.Parameters.Add(new SqlParameter("@EventId", SqlDbType.UniqueIdentifier)).Value = eventId;
+                        command.Parameters.Add(new SqlParameter("@CreatedBy", SqlDbType.UniqueIdentifier)).Value = createdBy;
+
+                        // Output parameter for new register ID
+                        SqlParameter newRegisterIdParam = new SqlParameter("@NewRegisterId", SqlDbType.UniqueIdentifier)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+                        command.Parameters.Add(newRegisterIdParam);
+
+                        // Execute the command
+                        await command.ExecuteNonQueryAsync();
+
+                        // Retrieve the output value (if any)
+                        if (newRegisterIdParam.Value != DBNull.Value)
+                        {
+                            newRegisterId = (Guid?)newRegisterIdParam.Value;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return newRegisterId;
         }
     }
 }
